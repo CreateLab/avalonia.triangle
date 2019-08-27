5c88ff2c3d1cf7c81971b832aff37d0952e7f74c
Lary <f98f96@gmail.com>
2019-08-27 17:58:47 +0300

    2nd

--- Avalonia.NETCoreMVVMApp1/Avalonia.NETCoreMVVMApp1/ViewModels/MainWindowViewModel.cs
+++ Avalonia.NETCoreMVVMApp1/Avalonia.NETCoreMVVMApp1/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Media;
@@ -15,75 +16,132 @@ namespace Avalonia.NETCoreMVVMApp1.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private Dot _startDot= new Dot();
-        private Dot _firstDot= new Dot();
-        private Dot _secondDot= new Dot();
-        private Dot _thirdDot= new Dot();
-
+        private Dot _startDot = new Dot();
+        private Dot _firstDot = new Dot();
+        private Dot _secondDot = new Dot();
+        private Dot _thirdDot = new Dot();
+        private bool stat;
         public int StartDotX
         {
-            get =>(int) _startDot.X;
+            get => (int) _startDot.X;
             set => this.RaiseAndSetIfChanged(ref _startDot.X, value);
         }
+
         public int StartDotY
         {
-            get =>(int) _startDot.Y;
+            get => (int) _startDot.Y;
             set => this.RaiseAndSetIfChanged(ref _startDot.Y, value);
         }
+
         public int FirstDotX
         {
-            get => (int)_firstDot.X;
+            get => (int) _firstDot.X;
             set => this.RaiseAndSetIfChanged(ref _firstDot.X, value);
         }
+
         public int FirstDotY
         {
-            get => (int)_firstDot.Y;
+            get => (int) _firstDot.Y;
             set => this.RaiseAndSetIfChanged(ref _firstDot.Y, value);
         }
+
         public int SecondDotX
         {
-            get => (int)_secondDot.X;
+            get => (int) _secondDot.X;
             set => this.RaiseAndSetIfChanged(ref _secondDot.X, value);
         }
+
         public int SecondDotY
         {
-            get => (int)_secondDot.Y;
+            get => (int) _secondDot.Y;
             set => this.RaiseAndSetIfChanged(ref _secondDot.Y, value);
         }
+
         public int ThirdDotX
         {
-            get =>(int) _thirdDot.X;
+            get => (int) _thirdDot.X;
             set => this.RaiseAndSetIfChanged(ref _thirdDot.X, value);
         }
+
         public int ThirdDotY
         {
-            get => (int)_thirdDot.Y;
+            get => (int) _thirdDot.Y;
             set => this.RaiseAndSetIfChanged(ref _thirdDot.Y, value);
         }
 
-       // private SourceList<IControl> _controls;
-
-        //public IObservable<IChangeSet<IControl>> Connect() => _controls.Connect();
-        //public ReactiveList<IControl> MyCollection =>  new ReactiveList<IControl>{new Line{StartPoint = new Point(1,1),EndPoint = new Point(10,10),Stroke = Brushes.Aqua},new Line{StartPoint = new Point(1,1),EndPoint = new Point(1,10),Stroke = Brushes.Blue},  };
         SourceList<DotObject> dots = new SourceList<DotObject>();
         private ReadOnlyObservableCollection<DotObject> _collection;
         public ReadOnlyObservableCollection<DotObject> Collection => _collection;
+
         public MainWindowViewModel()
         {
+            _firstDot= new Dot{X = 200,Y=200};
+            _secondDot= new Dot{X=100,Y=300};
+            _thirdDot = new Dot{X=300,Y=300};
             dots.Connect().Bind(out _collection).Subscribe();
         }
 
         public void SetDots()
         {
+            stat = false;
             dots.Clear();
             dots.Add(new DotObject(_firstDot));
-            dots.Add(new DotObject( _secondDot));
-            dots.Add(new DotObject( _thirdDot));
-            dots.Add(new DotObject( _startDot));
-            // controls.Add(new Ellipse{RenderTransformOrigin = new RelativePoint(new Point(_firsttDot.X,_firsttDot.Y),RelativeUnit.Absolute ),Height = 5,Width = 5, Fill = Brushes.Red});
-            // controls.Add();
-            // controls.Add(new Ellipse{RenderTransformOrigin = new RelativePoint(new Point(_thirdDot.X,_thirdDot.Y),RelativeUnit.Absolute ),Height = 5,Width = 5, Fill = Brushes.Red});
-            //controls.Add(new Ellipse{RenderTransformOrigin = new RelativePoint(new Point(_startDot.X,_startDot.Y),RelativeUnit.Absolute ),Height = 5,Width = 5, Fill = Brushes.Blue});
+            dots.Add(new DotObject(_secondDot));
+            dots.Add(new DotObject(_thirdDot));
+            dots.Add(new DotObject(_startDot));
+        }
+
+        public void Clear()
+        {
+            stat = false;
+            dots.Clear();
+        }
+
+        public async Task Start()
+        {
+            stat = true;
+            var commonDot = _startDot;
+            await Task.Run(async () =>
+                {
+                    while (stat)
+                    {
+                        var rnd = new Random().Next(1, 4);
+                        switch (rnd)
+                        {
+                            case 1:
+                            {
+                                commonDot.X = (_firstDot.X + commonDot.X) / 2;
+                                commonDot.Y = (_firstDot.Y + commonDot.Y) / 2;
+                                break;
+                            }
+                            case 2:
+                            {
+                                commonDot.X = (_secondDot.X + commonDot.X) / 2;
+                                commonDot.Y = (_secondDot.Y + commonDot.Y) / 2;
+                                break;
+                            }
+                            case 3:
+                            {
+                                commonDot.X = (_thirdDot.X + commonDot.X) / 2;
+                                commonDot.Y = (_thirdDot.Y + commonDot.Y) / 2;
+                                break;
+                            }
+                        }
+                        //SourceList<DotObject> dots = new SourceList<DotObject>();
+                        dots.Add(new DotObject(commonDot));
+                        await Task.Delay(200);
+                    }
+                   
+                   
+                    
+                }
+            );
+        }
+        
+
+        public void Stop()
+        {
+            stat = false;
         }
     }
 }
